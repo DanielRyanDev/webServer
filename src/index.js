0de8040265d91ae51223bd1645b32b05bcdf2cf3
DanielRyanDev <daniel.jgryan2016@gmail.com>
2022-01-12 12:38:20 +0000

    correct syntactical issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const movieRouter = require("./movie/movieRoutes");
 const userRouter = require("./user/userRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use(movieRouter);
 app.use(userRouter);
 
